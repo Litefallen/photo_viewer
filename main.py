21d733dca12497dbb3ecc1d72c569c2ab1a09aff
Litefallen <nolife0808@gmail.com>
2022-09-09 18:36:04 +0200

    pics open correctly, only one pic widget window, need to correct position of pics window and align it with main root window, add scrollbar maybe

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
+import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 import os
-
+print(())
 root = Tk()
+
 root.title('Image Viewer')
+root.geometry(f'25x{root.winfo_screenheight()}+0+0') # moving root window to the left
 root.iconphoto(False, PhotoImage(file='image.png'))
 dir_list = []
 used_list = []
 forbidden_list = []
 pic_list = []
 pic_name_list = []
-
-# img = ImageTk.PhotoImage(
-#     Image.open('D:/music/Moby - All Visible Objects (2020)/sleeves/Moby - All Visible Objects (9).jpg'))
-#
-# img_widget = Label(image=img)
-# img_widget.pack()
-
+pic_container = None
+img_root = None
 
 def pic_opener(pic_path):
-    print(pic_path)
+    global pic_container, img_root
+    if img_root: # making only one pic window opened
+        img_root.destroy()
     img_root = Toplevel(root)
-    img = ImageTk.PhotoImage(Image.open(pic_path))
-    img_widget = Label(img_root, image=img)
-    img_widget.pack()
+    pic_container = ImageTk.PhotoImage(Image.open(pic_path))
+    img_root.geometry(f'{pic_container.width()}x{pic_container.height()}+100+0')
+    img_widget = Label(img_root, image=pic_container)
+    img_widget.grid()
 
 
 def permission_check(folder):  # check if we have access to the folder
@@ -56,24 +57,19 @@ def pic_finder(initial_folder):
 
 pic_finder('D:/music')
 
-# print(list(os.scandir('D:/music')))
-counter = 0
-# for pic in pic_list:
-#     # print(pic)
-#     pics = Button(root, text=pic[0], width=10, command=lambda: pic_opener(pic[1]))
-#     pics.pack(expand=TRUE, fill=X)
-#     counter += 1
-# img = ImageTk.PhotoImage(Image.open('D:/music/Moby - All Visible Objects (2020)/sleeves/Moby - All Visible Objects (9).jpg'))
-# img_widget = Label(image=img)
-# img_widget.pack()
-vv = Toplevel(root)
-img = ImageTk.PhotoImage(Image.open('D:/music/Moby - All Visible Objects (2020)/sleeves/Moby - All Visible Objects (9).jpg'))
-def bb(adress):
-    img_widget = Label(vv,image=img)
-    print(bool(img_widget))
-    img_widget.pack()
 
-a = Button(root,command=lambda :bb('D:/music/Moby - All Visible Objects (2020)/sleeves/Moby - All Visible Objects (9).jpg'))
-a.pack()
+class SpecificButton(Button):  # create custom class with fixed command function
+    def fixed_command(self, name, dest):
+        self.title = name
+        self.path = dest
+        fixed_but = Button(root, text=self.title, command=lambda: pic_opener(self.path),width=25)
+        fixed_but.pack(side=TOP)
+        print(fixed_but.winfo_screenwidth())
+
+
+# print(list(os.scandir('D:/music')))
+for pic in pic_list:
+    cl_but = SpecificButton()
+    cl_but.fixed_command(pic[0], pic[1])
 
 root.mainloop()

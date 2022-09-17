e7448a156c98fc5f32cb3a6e3da051128b88c328
Litefallen <nolife0808@gmail.com>
2022-09-17 19:53:13 +0200

    scroll works, need to alight everything properly and make pics to open inside main window  and not create new one

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-
 from tkinter import *
 from PIL import ImageTk, Image
 import os
 
 root = Tk()
-
 root.title('Image Viewer')
 sidebar_sizes = (int(root.winfo_screenwidth() / 8), int(root.winfo_screenheight() - root.winfo_screenheight() / 10))
-root.geometry(f'{sidebar_sizes[0]}x{sidebar_sizes[1]}+0+0')  # moving root window to the left and set its size
+# root.geometry(f'{sidebar_sizes[0]}x{sidebar_sizes[1]}+0+0')  # moving root window to the left and set its size
 root.iconphoto(False, PhotoImage(file='image.png'))
+
+# creating interface
+main_frame = Frame(root)
+main_frame.pack(fill=Y,expand=1)
+main_canvas = Canvas(main_frame)
+main_canvas.pack(side=LEFT,fill=BOTH,expand=1)
+
+scrollbar = Scrollbar(main_frame, command=main_canvas.yview)
+scrollbar.pack(side=RIGHT,fill=Y)
+main_canvas.configure(yscrollcommand=scrollbar.set)
+main_canvas.bind("<Configure>",lambda e: main_canvas.configure(scrollregion=main_canvas.bbox('all')))
+frame_with_buttons = Frame(main_canvas,bg="blue")
+
+main_canvas.create_window((0,0),window=frame_with_buttons,anchor=NW)
+
+# back end stuff
 
 dir_list = []
 used_list = []
@@ -18,6 +32,7 @@ pic_name_list = []
 pic_container = None
 img_root = None
 last_folder = None
+
 
 def pic_opener(pic_path):
     global pic_container, img_root
@@ -55,35 +70,29 @@ def pic_finder(initial_folder):
         pic_path_add(initial_folder)  # get pics full path to list
         used_list.append(initial_folder)
     for folder in dir_list:
-            if folder not in used_list and folder not in forbidden_list:
-                pic_finder(folder)
+        if folder not in used_list and folder not in forbidden_list:
+            pic_finder(folder)
     return dir_list
 
-def recursion_reset_search(folder):
-    if RecursionError:
-        recursion_reset_search(last_folder)
-    else:
-        pic_finder(folder)
 
+# Buttons
 class SpecificButton(Button):  # create custom class with fixed command function
     def fixed_command(self, name, dest):
         self.title = name
         self.path = dest
-        fixed_but = Button(root, text=self.title, command=lambda: pic_opener(self.path), width=sidebar_sizes[0], anchor=W)
-        fixed_but.pack()
+        fixed_but = Button(frame_with_buttons, text=self.title, command=lambda: pic_opener(self.path),
+                           anchor=NW)
+        fixed_but.pack(fill=BOTH,expand=1)
 
 
-
-# recursion_reset_search('D:/music')
 pic_finder('D:/music')
 # print(list(os.scandir('D:/music')))
 
 
-root.bind('<Escape>',lambda e:root.quit())
+root.bind('<Escape>', lambda e: root.quit())
 
 for pic in pic_list:
     cl_but = SpecificButton()
     cl_but.fixed_command(pic[0], pic[1])
-
 
 root.mainloop()

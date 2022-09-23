6c24eed6a83e6c4f474b1c192a95601e7639a74a
Litefallen <nolife0808@gmail.com>
2022-09-23 22:51:03 +0200

    made pics zoom in or zoom out to cover as much window space as possible,without increasing window size, with correct ratio. Still need to fix recursion stack overload, and make graphic menu to select folder to search pics in

--- main.py
+++ main.py
@@ -10,22 +10,23 @@ sidebar_sizes = (int(root.winfo_screenwidth() / 8), int(root.winfo_screenheight(
 root.iconphoto(False, PhotoImage(file='image.png'))
 root.state("zoomed")
 # creating interface
-main_frame = Frame(root,bd=0)
+main_frame = Frame(root, bd=0)
 # main_frame.pack(fill=Y,expand=1)
-main_frame.grid(row=0,column=0,sticky=NSEW)
-main_canvas = Canvas(main_frame,width=int(root.winfo_screenwidth() / 8),height=int(root.winfo_screenheight() - root.winfo_screenheight() / 10))
+main_frame.grid(row=0, column=0, sticky=NSEW)
+main_canvas = Canvas(main_frame, width=int(root.winfo_screenwidth() / 8),
+                     height=int(root.winfo_screenheight() - root.winfo_screenheight() / 10))
 # main_canvas.grid(row=0,column=0,side=LEFT,fill=BOTH,expand=1)
-main_canvas.grid(row=0,column=0,sticky=NSEW)
+main_canvas.grid(row=0, column=0, sticky=NSEW)
 # scrollbar_frame = Frame(root,bd=0)
 # scrollbar_frame.grid(row=0,column=1,sticky=NS)
 scrollbar = ttk.Scrollbar(main_frame, command=main_canvas.yview)
-scrollbar.grid(row=0,column=1,sticky=NS)
+scrollbar.grid(row=0, column=1, sticky=NS)
 # scrollbar.grid(row=0,column=1,sticky=NS,)
 main_canvas.configure(yscrollcommand=scrollbar.set)
-main_canvas.bind("<Configure>",lambda e: main_canvas.configure(scrollregion=main_canvas.bbox('all')))
+main_canvas.bind("<Configure>", lambda e: main_canvas.configure(scrollregion=main_canvas.bbox('all')))
 frame_with_buttons = Frame(main_canvas)
 
-main_canvas.create_window((0,0),window=frame_with_buttons,anchor=NW)
+main_canvas.create_window((0, 0), window=frame_with_buttons, anchor=NW)
 
 # back end stuff
 
@@ -38,16 +39,40 @@ pic_container = None
 last_folder = None
 
 
+def pic_resizer(pic):
+    width, height = pic.width, pic.height
+    print(f' initial picture width is : {width} and initial height is {height}')
+    print(f' window width is : {(root.winfo_screenwidth() - root.winfo_screenwidth() / 8)} and window height is {root.winfo_screenheight() - root.winfo_screenheight() / 10}')
+
+    width_dif = ((root.winfo_screenwidth() - root.winfo_screenwidth() / 8) / width)
+    height_dif = ((root.winfo_screenheight() - root.winfo_screenheight() / 10) / height)
+    print(width_dif,height_dif)
+    resized_pic_width = int(width * min(width_dif,height_dif))
+    resized_pic_height = int(height * min(width_dif,height_dif))
+    # print(resized_pic_width,resized_pic_height)
+    print(resized_pic_width, resized_pic_height)
+    # print(main_canvas.winfo_screenheight(),main_canvas.winfo_screenwidth())
+    return resized_pic_width, resized_pic_height
+
+
 def pic_opener(pic_path):
     global pic_container
     # if img_root:  # making only one pic window opened
     #     img_root.destroy()
     # img_root = Toplevel(root)
     # pic_container = ImageTk.PhotoImage(Image.open(pic_path))
-    pic_container = ImageTk.PhotoImage(file=pic_path)
+
+    opened_pic = Image.open(pic_path)
+    # print(opened_pic.width,opened_pic.height)
+    # print((pic_resizer(opened_pic)))
+    opened_pic = opened_pic.resize((pic_resizer(opened_pic)))
+    # opened_pic = opened_pic.resize((1777,1777))
+    pic_container = ImageTk.PhotoImage(opened_pic)
     # img_root.geometry(f'{pic_container.width()}x{pic_container.height()}+{sidebar_sizes[0]}+0')
-    img_widget = Label(main_frame, image=pic_container,anchor=NW)
-    img_widget.grid(row=0,column=3,sticky=NSEW)
+    img_widget = Label(main_frame, image=pic_container, anchor=NW)
+
+    img_widget.grid(row=0, column=3, sticky=NSEW)
+    # print(pic_container.width(), pic_container.height())
 
 
 def permission_check(folder):  # check if we have access to the folder
@@ -86,7 +111,7 @@ class SpecificButton(Button):  # create custom class with fixed command function
         self.path = dest
         fixed_but = Button(frame_with_buttons, text=self.title, command=lambda: pic_opener(self.path),
                            anchor=NW)
-        fixed_but.pack(fill=BOTH,expand=1)
+        fixed_but.pack(fill=BOTH, expand=1)
 
 
 pic_finder('D:/music')
